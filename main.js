e45f81255dd734240b2ee9bece2886a013a688be
mark <markpackham1@gmail.com>
2024-06-27 07:44:36 +0100

    Test running Vite & Bun in Windows - https://bun.sh/guides/ecosystem/vite

---
+++ main.js
@@ -0,0 +1,26 @@
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <p> Running Vite off https://bun.sh/guides/ecosystem/vite </p>
+    <p> Cannot run <code>bunx</code> so had to change package.json to this <code>"dev": "bun --bun vite"</code> </p>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
